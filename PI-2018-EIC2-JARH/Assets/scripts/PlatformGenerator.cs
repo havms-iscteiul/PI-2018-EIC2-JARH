966fdfeb56f385de6fb26aeb9932725de955b4c0
Ana Nunes <arsns@iscte-iul.pt>
2018-11-25 17:56:32 +0000

    gerar tiles infinitos

---
+++ PI-2018-EIC2-JARH/Assets/scripts/PlatformGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+
+
+    public GameObject platform;
+    public Transform generationPoint;
+    public float distanceBetwen;
+    private float platformWith;
+    
+
+	// Use this for initialization
+	void Start () {
+        platformWith = platform.GetComponent<BoxCollider2D>().size.x;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.x < generationPoint.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + platformWith + distanceBetwen, transform.position.y, transform.position.z);
+            Instantiate(platform, transform.position, transform.rotation);
+        }
+
+	}
+}
