5d24b8f00b678bfe18d260623cc43face88ccb52
Ana Nunes <arsns@iscte-iul.pt>
2018-11-26 13:08:06 +0000

    tilles random

--- PI-2018-EIC2-JARH/Assets/scripts/GenerateRandoms.cs
+++ PI-2018-EIC2-JARH/Assets/scripts/GenerateRandoms.cs
@@ -9,24 +9,30 @@ public class GenerateRandoms : MonoBehaviour {
     private Cenarios cenarioSelecionado;
 
     public GameObject bg;
-    //public GameObject tille;
+    public GameObject tille;
+    public Sprite deserto;
+    public Sprite noturno;
+    public Sprite gelado;
+    public Sprite floresta;
     
     // Use this for initialization
     void Start () {
-        //tille.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Tilles/spitesheet_ground_23");
         cenarioSelecionado = (Cenarios)System.Enum.GetValues(typeof(Cenarios)).GetValue(DiscreteUniform.Sample(0, 3));
         if(cenarioSelecionado==Cenarios.Deserto){
             bg.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Backgrounds/Deserto");
-            
+            tille.GetComponent<SpriteRenderer>().sprite = deserto;
         }
         if (cenarioSelecionado == Cenarios.Noturno){
             bg.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Backgrounds/Noturno");
+            tille.GetComponent<SpriteRenderer>().sprite = noturno;
         }
         if (cenarioSelecionado == Cenarios.Gelado){
             bg.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Backgrounds/Gelado");
+            tille.GetComponent<SpriteRenderer>().sprite = gelado;
         }
         if (cenarioSelecionado == Cenarios.Floresta){
             bg.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Backgrounds/Floresta");
+            tille.GetComponent<SpriteRenderer>().sprite = floresta;
         }
     }
 	
