e5610eb615644e0a947536bff613af8b86dec28a
Ana Nunes <arsns@iscte-iul.pt>
2018-11-26 11:51:46 +0000

    backgrounds random

--- PI-2018-EIC2-JARH/Assets/scripts/GenerateRandoms.cs
+++ PI-2018-EIC2-JARH/Assets/scripts/GenerateRandoms.cs
@@ -8,11 +8,26 @@ public class GenerateRandoms : MonoBehaviour {
 
     private Cenarios cenarioSelecionado;
 
-    private GameObject plataform;
+    public GameObject bg;
+    //public GameObject tille;
     
     // Use this for initialization
     void Start () {
+        //tille.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Tilles/spitesheet_ground_23");
         cenarioSelecionado = (Cenarios)System.Enum.GetValues(typeof(Cenarios)).GetValue(DiscreteUniform.Sample(0, 3));
+        if(cenarioSelecionado==Cenarios.Deserto){
+            bg.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Backgrounds/Deserto");
+            
+        }
+        if (cenarioSelecionado == Cenarios.Noturno){
+            bg.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Backgrounds/Noturno");
+        }
+        if (cenarioSelecionado == Cenarios.Gelado){
+            bg.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Backgrounds/Gelado");
+        }
+        if (cenarioSelecionado == Cenarios.Floresta){
+            bg.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Backgrounds/Floresta");
+        }
     }
 	
 	// Update is called once per frame
